b28c8915c4aa0f3a76ef4ccfeee263552fe29efe
dalvarez-2022277 <dalvarez-2022277@kinal.edu.gt>
2024-05-02 16:49:15 -0600

    se agrega validacion de busqeuda por coincidencia

---
+++ Backend/src/Middleware/validarBusquedaCoincidencia.js
@@ -0,0 +1,15 @@
+
+const searchValidation = (req, res, next) => {
+  const searchTerm = req.query.search ? req.query.search.toLowerCase() : '';
+
+  if (searchTerm.trim() !== '') {
+    const filteredData = data.filter(item =>
+      item.toLowerCase().includes(searchTerm)
+    );
+    req.filteredData = filteredData;
+  } else {
+    req.filteredData = data;
+  }
+
+  next();
+};
